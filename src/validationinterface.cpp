a591fb8e223935504a7c3662e18f933c983c8af5
MGPC <53425811+mgpc-lab@users.noreply.github.com>
2023-03-20 08:31:49 +0800

    Fix the compiling error on Ubuntu 22.04 LTS
    Fix the compiling error on Ubuntu 22.04 LTS

--- src/validationinterface.cpp
+++ src/validationinterface.cpp
@@ -4,7 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <validationinterface.h>
-
+#include <boost/bind.hpp>
 #include <primitives/block.h>
 #include <scheduler.h>
 #include <sync.h>
